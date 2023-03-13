e056fb7e5d2780d359511db33fd0503fce33dd81
Josymar Escalona Rodriguez <josymar.escalona@indra-avitech.aero>
2023-03-13 15:38:11 +0100

    product details components finished and package.json finished

--- components/Product.jsx
+++ components/Product.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { urlFor } from "../lib/client";
 
 const Product = ({ product: { image, name, slug, price } }) => {
+  
   return (
     <div>
       <Link href={`/product/${slug.current}`}>
