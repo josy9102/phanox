f1a70d78a2f98c71971df9bd5fa7c512ecf413eb
Josymar Escalona Rodriguez <josymar.escalona@indra-avitech.aero>
2023-03-13 13:17:07 +0100

    hero banner component created

--- pages/index.js
+++ pages/index.js
@@ -4,26 +4,38 @@ import React from "react";
 import { Product, FooterBanner, HeroBanner } from "../components";
 import { client } from "../lib/client";
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   //const { Title, Text } = Typography; // do it later on
 
   const dummyProducts = ["Product 1", "Product 2"];
 
   return (
     <>
-      <HeroBanner>Hero Banner</HeroBanner>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}></HeroBanner>
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {dummyProducts.map((product) => (
-          <Product>{product}</Product>
+        {products?.map((product) => (
+          <Product>{product.name}</Product>
         ))}
       </div>
       <FooterBanner>Footer</FooterBanner>
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const productQuery = '*[_type == "product"]';
+  const products = await client.fetch(productQuery);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
